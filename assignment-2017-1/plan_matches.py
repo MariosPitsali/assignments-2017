af4df2ea07a902f159cb95eaea9e168d1bb74468
MariosPitsali <marios.pitsali@gmail.com>
2017-03-29 20:22:22 +0300

    etc

--- assignment-2017-1/plan_matches.py
+++ assignment-2017-1/plan_matches.py
@@ -3,16 +3,19 @@ input_filename = "graph_file.txt"
 g = {}
 with open(input_filename) as graph_input:
     for line in graph_input:
-        # split line and convert line parts to integers
-        nodes = [int(x) for x in line.split()]
+        nodes = [str(x) for x in line.split()]
+        length = len(nodes)
         if len(nodes) != 2:
             continue
-        # if a node is not already in the graph
-        # we must create a new empty list 
         if nodes[0] not in g:
             g[nodes[0]] = []
         if nodes[1] not in g:
             g[nodes[1]] = []
-        # we need to append the "to" node
-        # to the existing list for the "from" node
         g[nodes[0]].append(nodes[1])
+m = {}
+for i in range(0,length):
+    m[i]=[]
+
+for key in g:
+    for i in key:
+        
